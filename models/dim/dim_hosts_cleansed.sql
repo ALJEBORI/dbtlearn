e920193e570d191c4b9dbba01ee7dae5b3600081
MohALJ <moh_aljebori@yahoo.com>
2025-04-08 23:55:02 +0200

    Incremental Model

---
+++ models/dim/dim_hosts_cleansed.sql
@@ -0,0 +1,11 @@
+WITH scr_hosts AS (
+    select * from {{ ref('src_hosts') }}
+)
+
+SELECT
+host_id,
+NVL(host_name,'Anonymous') as host_name,
+IS_SUPERHOST,
+CREATED_AT,
+UPDATED_AT
+FROM scr_hosts
