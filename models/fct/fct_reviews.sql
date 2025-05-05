26f189aa1647807b70b273a12311f4569eb1d339
MohALJ <moh_aljebori@yahoo.com>
2025-05-05 23:59:28 +0200

    variabls range

--- models/fct/fct_reviews.sql
+++ models/fct/fct_reviews.sql
@@ -14,5 +14,12 @@ select
 from src_reviews
 where  review_text is not null
 {% if is_incremental()  %}
-    AND review_date > coalesce((select max(review_date) from {{ this }}), '1900-01-01')
+  {% if var("start_date",False) and var("end_date",False)  %}
+    {{ log('Loading ' ~ this ~ 'incrementally (start_date: ' ~ var("start_date") ~ ', end_date: ' ~ var("end_date") ~ ')', info=True) }}
+     AND review_date>= '{{var("start_date")}}'
+     AND review_date<'{{var("end_date") }}'
+  {% else %}
+    AND review_date>(select max(review_date) from {{ this }})
+    {{ log('Loading ' ~ this ~ 'incrementally ( all missing dates)', info=True)}}
+  {% endif %}
 {% endif %}
